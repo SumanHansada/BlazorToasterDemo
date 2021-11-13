2d5e020ee52f2dacd06fd6ee98f6a4bcd9d61869
Suman Hansada <suman.hansada@gmail.com>
2021-11-13 10:33:52 +0530

    Fixed initialization issue in Toaster Component

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -8,19 +8,28 @@ namespace BlazorToasterDemo.Pages
 {
     public partial class Index
     {
-        protected IEnumerable<ToastButton> buttons = new[] {
-            new ToastButton { Id = 1, Type = "success", ClassName = "success", Label = "Success", Description = "This is a success toast component"},
-            new ToastButton { Id = 2, Type = "danger", ClassName = "danger", Label = "Danger", Description = "This is a danger toast component"},
-            new ToastButton { Id = 3, Type = "info", ClassName = "info", Label = "Info", Description = "This is a info toast component"},
-            new ToastButton { Id = 4, Type = "warning", ClassName = "warning", Label = "Warning", Description = "This is a warning toast component"},
-        };
-
-        public string position = "Select Position";
-        public bool autoDismiss = false;
-        public int autoDismissTime = 0;
 
         //reference for Toaster Component
         Toaster toaster;
+
+        public IEnumerable<ToastButton> buttons { get; set; }
+        public string position { get; set; }
+        public bool autoDismiss { get; set; }
+        public int autoDismissTime { get; set; }
+
+        protected override void OnInitialized()
+        {
+            buttons = new List<ToastButton> {
+                new ToastButton { Id = 1, Type = "success", ClassName = "success", Label = "Success", Description = "This is a success toast component"},
+                new ToastButton { Id = 2, Type = "danger", ClassName = "danger", Label = "Danger", Description = "This is a danger toast component"},
+                new ToastButton { Id = 3, Type = "info", ClassName = "info", Label = "Info", Description = "This is a info toast component"},
+                new ToastButton { Id = 4, Type = "warning", ClassName = "warning", Label = "Warning", Description = "This is a warning toast component"},
+            };
+
+            position = "Select Position";
+            autoDismiss = false;
+            autoDismissTime = 0;
+        }
 
         protected void AutoDismissToggle()
         {
@@ -28,7 +37,8 @@ namespace BlazorToasterDemo.Pages
             toaster.ClearToasts();
         }
 
-        protected void SelectPosition(ChangeEventArgs e) {
+        protected void SelectPosition(ChangeEventArgs e)
+        {
             position = e.Value.ToString();
             toaster.ClearToasts();
         }
